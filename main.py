43f58b891e0729f5e20a4f4970e8cf4dad8d738b
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2023-02-25 14:51:34 +0530

    added search feature

--- main.py
+++ main.py
@@ -7,20 +7,19 @@ app = Flask(__name__,
             static_url_path='/static', 
             static_folder='static')
 
-
-invidious_base_url = 'https://inv.riverside.rocks'
+invidious_base_url = 'https://invidious.baczek.me/'
 
 def get_invidious_url():
     # inv_api="https://api.invidious.io/instances.json?pretty=1&sort_by=api,health"
     # r = requests.get(inv_api)
     # data = r.json()
     #return(data[0][1]['uri'])
-    return "https://inv.riverside.rocks/"
+    return "https://invidious.baczek.me/"
 
 @app.route('/')
 def index():
     courses = deta.Base('courses').fetch().items
-    invidious_base_url = get_invidious_url()
+    #invidious_base_url = get_invidious_url()
     for i in courses:
         i['complete_percentage'] = round((len(i['watched'])/i['videoCount'])*100)
     return render_template('dashboard.html', courses=courses)
@@ -38,7 +37,7 @@ def new_course():
         else:
             playlist_id= url.split('=')[1]
             invidious_base_url = get_invidious_url()
-            invidious_url = f'{invidious_base_url}/api/v1/playlists/{playlist_id}'
+            invidious_url = f'{invidious_base_url}api/v1/playlists/{playlist_id}'
             r = requests.get(invidious_url)
             data = r.json()
             watched = {'watched': []}
@@ -47,6 +46,20 @@ def new_course():
             base = deta.Base('courses')
             base.put(data)
             return jsonify({'success': True})
+
+@app.route('/course/new_from_id', methods=['POST'])
+def new_from_id():
+    playlist_id = request.form['playlist_id']
+    invidious_base_url = get_invidious_url()
+    invidious_url = f'{invidious_base_url}api/v1/playlists/{playlist_id}'
+    r = requests.get(invidious_url)
+    data = r.json()
+    watched = {'watched': []}
+    data.update(watched)
+    data['playlistThumbnail'] = data['playlistThumbnail'].replace('hqdefault', 'maxresdefault')
+    base = deta.Base('courses')
+    base.put(data)
+    return redirect('/')
 
 @app.route('/course/<id>', methods=['GET'])
 def course(id):
@@ -98,7 +111,8 @@ def video(id, video_id):
     course = deta.Base('courses').fetch({'key': id}).items[0]
     all_videos = course['videos']
     video = [i for i in all_videos if i['videoId'] == video_id][0]
-    video_api_url = f'{invidious_base_url}/api/v1/videos/{video_id}'
+    video_api_url = f'{invidious_base_url}api/v1/videos/{video_id}'
+    print(video_api_url)
     r = requests.get(video_api_url)
     video_data = r.json()
     description = video_data['description'].replace('\n', '<br>')
@@ -127,7 +141,7 @@ def actions():
         for i in all_courses:
             playlist_id = i['playlistId']
             invidious_base_url = get_invidious_url()
-            invidious_url = f'{invidious_base_url}/api/v1/playlists/{playlist_id}'
+            invidious_url = f'{invidious_base_url}api/v1/playlists/{playlist_id}'
             r = requests.get(invidious_url)
             data = r.json()
             videos = data['videos']
@@ -135,3 +149,30 @@ def actions():
             base = deta.Base('courses')
             base.update({'videos': videos, 'videoCount': video_count}, i['key'])
         return jsonify({'success': True})
+
+@app.route("/search", methods=['GET'])
+def search():
+    query = request.args.get('q')
+    invidious_base_url = get_invidious_url()
+    search_url = f"{invidious_base_url}api/v1/search?q={query}&type=playlist"
+    r = requests.get(search_url)
+    data = r.json()
+    return render_template('search.html',inv_url=invidious_base_url, results=data, q=query)
+
+@app.route("/api/search/suggestions", methods=['GET'])
+def search_suggestions():
+    query = request.args.get('q')
+    invidious_base_url = get_invidious_url()
+    search_url = f'{invidious_base_url}api/v1/search/suggestions?q={query}'
+    r = requests.get(search_url)
+    data = r.json()
+    suggestions = data['suggestions']
+    return jsonify(suggestions)
+
+
+@app.route("/course/search", methods=['GET'])
+def search_course():
+    return render_template('search.html')
+
+def search_results(data):
+    return render_template('search_results.html', results=data)
